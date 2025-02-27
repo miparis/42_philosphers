5c337841a5a2f1bc305f2025487f9114ce3d1cb5
Miranda Paris <miparis@c1r15s6.42madrid.com>
2025-02-27 10:31:27 +0100

    Makefile & .h

---
+++ inc/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miparis <miparis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 14:09:35 by miparis           #+#    #+#             */
+/*   Updated: 2025/02/20 14:23:54 by miparis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PHILO_H
+# define PHILO_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+
+#endif
